97aeef059e18084e6d7fbd70f1bb5fb8dd0af505
Strong-1210 <codeninja0912@gmail.com>
2024-12-13 21:12:22 -0900

    first commit

---
+++ Sentiment.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import nltk
+nltk.download('punkt_tab')
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+
+df = pd.read_csv('E:\My project\Sentiment\Tweets.csv')
+df.head()
+
+# Convert text to lowercase
+df['text'] = df['text'].str.lower()
+
+df['text'] = df['text'].astype(str)  # Convert 'text' column to string data type
+
+df['tokens'] = df['text'].apply(nltk.word_tokenize)  # Tokenization
+
+# Remove stopwords
+stopwords = nltk.corpus.stopwords.words('english')
+df['tokens'] = df['tokens'].apply(lambda x: [word for word in x if word not in stopwords])
+
+X = df['text']
+y = df['sentiment']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+vectorizer = TfidfVectorizer()
+X_train_vectors = vectorizer.fit_transform(X_train)
+X_test_vectors = vectorizer.transform(X_test)
+
+model = SVC()
+model.fit(X_train_vectors, y_train)
+
+y_pred = model.predict(X_test_vectors)
+
+print("Classification Report:")
+print(classification_report(y_test, y_pred))
+
+print("Accuracy Score:", accuracy_score(y_test, y_pred))
